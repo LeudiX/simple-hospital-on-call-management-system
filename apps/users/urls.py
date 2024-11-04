7855b2f68850d6fab4c5d0d5d55227a90706f0da
LeudiX <leudix.rafael@gmail.com>
2024-11-03 20:39:43 -0700

    Improved UI of users  on system management

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
     path('register/',views.register, name='register'), # URL for users registration
-    path('users/admin',views.list_users, name='users'), # URL  for existing users list view
+    path('users/admin/',views.list_users, name='users'), # URL  for existing users list view
     path('users/<int:id>/edit/',views.edit_user, name='edit_user'), # URL for updating a user
     path('users/<int:id>/delete/', views.delete_user, name='delete_user'),  # URL for deleting a user
     path('users/delete-users/confirm', views.delete_users, name='delete_users'), # URL for handle multiple user removal
